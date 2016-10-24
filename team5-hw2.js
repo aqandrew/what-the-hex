b4584d0368ddc5827dded280743d5e19453626c9
dawneraq <dawneraq@gmail.com>
2016-10-24 13:47:51 -0400

    Add scoring function

--- team5-hw2.js
+++ team5-hw2.js
@@ -8,6 +8,8 @@ $.fn.hexed = function (settings) {
 	}
 	
 	var actualColor = randomColor();
+	
+	// Gameplay logic goes here
 
 	return this;
 };
@@ -28,7 +30,19 @@ function randomColor() {
 	};
 }
 
-// TODO scoring function
+function getScore(expectedColor, actualColor, timeElapsed, settings) {
+	var percentageOff = 0;
+	
+	for (var component in expectedColor) {
+		percentageOff += Math.abs(expectedColor[component] - actualColor[component]) / 255 * 100;
+	}
+	
+	percentageOff /= 3;
+	var score = (((15 - settings.difficulty - percentageOff) / (15 - settings.difficulty)) * (15000 - timeElapsed)).toFixed(2);
+	
+	return score;
+}
+
 // TODO CheckIt! button
 // TODO jQuery UI sliders which update RGB values
 // TODO challenge
